359b1a4b993887c772934758024bcf4170a10dda
SuperCoder <robinstumpf01@gmail.com>
2024-07-18 15:27:41 +0200

    done

---
+++ src/01_basic_ts/basic.ts
@@ -0,0 +1,88 @@
+console.log('Welcome :)');
+
+let dogName = "Leon";
+
+console.log(dogName);
+
+dogName = "elf";
+
+let seventeen = 17;
+
+const sum = seventeen +44;
+
+console.log(sum);
+
+let num = 1;
+
+console.log("addition: ", 1 + num);
+console.log("substraction: ", 2 - num);
+console.log("division: ", 2 / num);
+console.log("modulus: ", 40 % 9);
+
+const x = 20;
+const y = 30;
+
+const addition = x + y;
+console.log(addition)
+
+const substraction1 = x - y;
+console.log(substraction1);
+
+const substraction2 = y - x;
+console.log(substraction2);
+
+const multiplication = x * y;
+console.log(multiplication);
+
+const division = x / y;
+console.log(division);
+
+const z = 10;
+
+const resultOne = x * y / z;
+console.log(resultOne);
+
+let a = 15;
+let b = 9;
+
+console.log(a % b);
+
+const c = 20;
+const resultTwo = (a + b) * c;
+console.log(resultTwo)
+
+ a++;
+console.log(a);
+
+b --;
+console.log(b);
+
+const resultThree = b - a;
+console.log(resultThree);
+
+console.log(resultOne % resultTwo);
+
+let score: number = 5 + 5 * 10;  
+console.log('Ergebnis: ' + score);
+
+score = 0;
+score = (5 + 5) * 10;  
+console.log('Ergebnis: ' + score);
+
+score = 0;  
+score = score + 10;  
+console.log('Ergebnis: ' + score);
+
+score = 0;
+score += 10;  
+console.log('Ergebnis: ' + score);
+
+let zahl = 1;  
+zahl = zahl + 1;  
+zahl += 1;
+
+zahl++;  
+console.log('increment: ' + zahl);
+
+zahl--;  
+console.log('dekrement: ' + zahl);
