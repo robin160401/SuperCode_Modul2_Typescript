50f482806697ce71d6471bd2f6f482a38864b463
SuperCoder <robinstumpf01@gmail.com>
2024-07-26 15:33:19 +0200

    bonus

---
+++ src/07_Aufgaben/mealplanner/mealplanner.ts
@@ -0,0 +1,72 @@
+ const mealInput = document.getElementById('mealInput') as HTMLInputElement;
+ const weekSelector = document.getElementById('weekSelector') as HTMLSelectElement;
+ const mealList = document.getElementById('mealList') as HTMLUListElement;
+ const mealForm = document.getElementById('mealForm') as HTMLFormElement;
+
+
+const arrayWeek1: string[] = ["hi1"];
+const arrayWeek2: string[] = ["hi2"];
+const arrayWeek3: string[] = ["hi3"];
+const arrayWeek4: string[] = ["hi4"];
+
+console.log(weekSelector)
+
+function addMeal(event: Event){
+    event.preventDefault();
+    const meal = mealInput.value;
+    if (mealInput){
+        if (weekSelector){
+            const week = weekSelector.value.toString();
+            switch (week){
+                case "week2":
+                    arrayWeek2.push(meal);
+                    break
+                case "week3":
+                    arrayWeek3.push(meal);
+                    break;
+                case "week4":
+                    arrayWeek3.push(meal);
+                    break
+                default:
+                    arrayWeek1.push(meal);
+            }
+        }
+        mealInput.value = "";
+        update(weekSelector.value);
+    }
+}
+
+function update(week: string) {
+    mealList.innerHTML = "";
+    let currentWeek: string[] = [];
+    switch (week) {
+        case "week2":
+            currentWeek = arrayWeek2;
+            break;
+        case "week3":
+            currentWeek = arrayWeek3;
+            break;
+        case "week4":
+            currentWeek = arrayWeek4;
+            break;
+        default:
+            currentWeek = arrayWeek1;
+    }
+
+    currentWeek.forEach((meal) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = meal;
+        mealList.appendChild(listItem);
+    });
+}
+
+function changeWeek() {
+    if (weekSelector) {
+        update(weekSelector.value);
+    }
+}
+
+mealForm.addEventListener('submit', addMeal);
+weekSelector.addEventListener('change', changeWeek);
+
+
